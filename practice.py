7541e023362b9ff7311305abbac76d9458b51d9c
Matt Smith <smithmw99@gmail.com>
2022-04-01 17:13:23 -0600

    Page navigation

--- practice.py
+++ practice.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -15,17 +16,27 @@ print(driver.title)
 # Search bar has name 'q' in HTML
 search = driver.find_element_by_name("q")
 
+# Clear search bar
+search.clear()
+
 # Send inpunt to search bar
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
 # Wait for page to load elements
 try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Fast.com: Internet Speed Test"))
     )
-    print(main.text)
+    element.click()
+    #link = driver.find_element_by_partial_link_text("Fast.com: Internet Speed Test")
+    #link.click()
+
+    time.sleep(5)
+
+    driver.back()
+    driver.back()
+
+    time.sleep(5)
 finally:
     driver.quit()
-
-#main = driver.find_element_by_id("main")
