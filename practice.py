593ddb6ced673307d19662e885c40971c714c288
Matt Smith <smithmw99@gmail.com>
2022-03-31 23:59:19 -0600

    Use search results

--- practice.py
+++ practice.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -12,10 +15,17 @@ print(driver.title)
 # Search bar has name 'q' in HTML
 search = driver.find_element_by_name("q")
 
+# Send inpunt to search bar
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
 
-time.sleep(10)
+# Wait for page to load elements
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    print(main.text)
+finally:
+    driver.quit()
 
-#driver.close()
-driver.quit()
+#main = driver.find_element_by_id("main")
